9aca0786189539cf693368458865a391fb4899c2
fady <gamilfady605@gmail.com>
2023-03-24 16:00:09 +0200

    add async-handler to handle errors and fire the error-handler-middleware for errors after refactoring to async approach

--- controllers/Thread.ts
+++ controllers/Thread.ts
@@ -7,17 +7,18 @@ import crypto from "crypto";
 import { Thread } from "../types/Thread";
 import { ListThreadsResponse } from '../DTOs/Thread/ListThreadsResponse';
 
+// URL =>  GET  `/5000/threads/list`
 // => the received req type is an object 
 // => the response type is an object
-export const ListAllThreadsHandler: CustomRequestHandler<{}, ListThreadsResponse> = (req, res, next) =>
+export const ListAllThreadsHandler: CustomRequestHandler<{}, ListThreadsResponse> = async (req, res, next) =>
 {
-    throw new Error("Error while listing threads");
-    return res.status(200).json({ threads: db.ListAllThreads() });
+    return res.status(200).json({ threads: await db.ListAllThreads() });
 };
 
+// URL =>  POST  `/5000/threads/new`
 // => receive an object represents the props we need from the thread type  
 // => return nothing
-export const CreateThread: CustomRequestHandler<CreateThreadRequest, CreateThreadResponse> = (req, res) =>
+export const CreateThread: CustomRequestHandler<CreateThreadRequest, CreateThreadResponse> = async (req, res) =>
 {
     // validate the dto recieved from the client 
     if (!req.body.title || !req.body.url || !req.body.userId) return res.status(400);
@@ -31,7 +32,7 @@ export const CreateThread: CustomRequestHandler<CreateThreadRequest, CreateThrea
         createdAt: Date.now(),
         isRemoved: false
     };
-    db.CreateNewThread(newThread);
+    await db.CreateNewThread(newThread);
     // returen the response status code
     return res.sendStatus(200);
 };
--- index.ts
+++ index.ts
@@ -2,14 +2,15 @@ import express from "express";
 import { logger } from "./logger";
 import { CreateThread, ListAllThreadsHandler } from './controllers/Thread';
 import { ErrorHandler } from "./Errors/ErrorHandler";
+import asyncHandler from "express-async-handler";
 
 const app = express();
 app.use(express.json());
 
 app.use(logger);
 
-app.get("/threads/list", ListAllThreadsHandler);
-app.post("/threads/new", CreateThread);
+app.get("/threads/list", asyncHandler(ListAllThreadsHandler));
+app.post("/threads/new", asyncHandler(CreateThread));
 
 app.get("/", (req, res, next) =>
 {
